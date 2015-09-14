8fd8d9f9a8e6739dc6f5c0976349cbc38278e211
matteosamaranch <matteosamaranch@gmail.com>
2015-09-14 09:36:21 +0200

    Initial Commit

---
+++ Game of Thrones App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Game of Thrones App
+//
+//  Created by Mateo Mauricio Samaranch on 14/09/15.
+//  Copyright (c) 2015 matsam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Game of Thrones App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Game of Thrones App
+//
+//  Created by Mateo Mauricio Samaranch on 14/09/15.
+//  Copyright (c) 2015 matsam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
